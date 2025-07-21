608392c09ee9581d84520ad9b29ac1b3a95437f5
oowixj819 <hychoi028@gmail.com>
2025-07-21 14:50:28 +0900

    [hoyeon] edit settings.py

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'static_collection'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATIC_URL = 'static/'
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -143,4 +148,3 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
